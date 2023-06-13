303c7d1b52d8e7fc2057e3446086739f5d45d085
cryptod0c <cryptod00c@gmail.com>
2023-06-12 21:36:15 -0400

    feat: tables complete

--- apps/org/components/ui-client/contractCopy.tsx
+++ apps/org/components/ui-client/contractCopy.tsx
@@ -5,8 +5,8 @@ export function ContractCopy(props: { contract: string; name?: string }) {
 
   return (
     <span className="flex items-center">
-      <span className="mr-1 dark:opacity-40 opacity-60">{name}:</span>
-      <span className="dark:opacity-50 opacity-70 flex items-center">
+      <span className="mr-1 opacity-60">{name}:</span>
+      <span className="opacity-70 flex items-center">
         <span className="hidden sm:block">{contract}</span>
         <span className="ml-1 relative inline-block top-0.5">
           <CopyButton title="" size={4} content={contract} />
