0555cea03ff25d7dc7fbcc73d99a3a971b329810
cryptod0c <cryptod00c@gmail.com>
2023-06-29 00:46:37 -0400

    chore: cleanup some responsive layout nits

--- apps/org/app/(marketing)/(blog.components)/base.tsx
+++ apps/org/app/(marketing)/(blog.components)/base.tsx
@@ -25,17 +25,19 @@ export function BlogBase(props: BlogBaseProps) {
   return (
     <>
       <div className="relative mx-auto max-w-8xl justify-center sm:px-2 lg:px-8 xl:px-12">
-        <div className="flex-auto w-full min-w-0 px-4 sm:pb-16 overflow-auto max-w-none lg:pl-8 lg:pr-0 xl:px-16">
-          <Link
-            className="absolute -top-14 text-sm dark:text-zinc-400 dark:hover:text-zinc-200 text-zinc-600 hover:text-zinc-800"
-            href="/blog"
-          >
-            ← Back to Blog
-          </Link>
+        <div className="flex-auto w-full min-w-0 px-2 sm:pb-16 overflow-auto max-w-none lg:pl-8 lg:pr-0 xl:px-16">
           <Prose as="div" className="max-w-[1200px]">
             <>
+              <div>
+                <Link
+                  className="absolute -top-14 xl:left-28 left- text-sm dark:text-zinc-400 dark:hover:text-zinc-200 text-zinc-600 hover:text-zinc-800"
+                  href="/blog"
+                >
+                  ← Back to Blog
+                </Link>
+              </div>
               {title && (
-                <header className="space-y-1 mb-9 md:mx-auto lg:mx-0">
+                <header className="space-y-1 sm:mb-9 mb-6 md:mx-auto lg:mx-0">
                   {!!tags?.length && (
                     <div className="flex flex-wrap items-center space-x-2 mb-6">
                       {tags.map((tag: string, key: number) => {
@@ -51,29 +53,36 @@ export function BlogBase(props: BlogBaseProps) {
                         )
                       })}
                       {date && (
-                        <h2 className="text-[14px] tracking-tight mt-0 mb-0 font-display text-zinc-500">
+                        <h2 className="text-[14px] tracking-tight font-normal mt-0 mb-0 font-display text-zinc-500">
                           {date}
                         </h2>
                       )}
                     </div>
                   )}
                   {title && (
-                    <h1 className="text-5xl font-bold font-heading">{title}</h1>
+                    <h1 className="xl:text-5xl text-3xl font-bold font-heading">
+                      {title}
+                    </h1>
                   )}
                 </header>
               )}
               {summary && (
-                <div className="text-2xl tracking-tighter text-zinc-500 leading-8 md:mx-auto lg:mx-0">
+                <div className="xl:text-2xl text-[16px] tracking-tighter text-zinc-500 leading-8 md:mx-auto lg:mx-0">
                   {summary}
                 </div>
               )}
             </>
+            <div className="pt-4 xl:hidden block">
+              <h3 className="text-sm font-normal leading-5 text-zinc-600 dark:text-zinc-400">
+                Posted by
+              </h3>
+            </div>
           </Prose>
 
           <div className="grid grid-cols-4 w-full mx-auto max-w-[1200px]">
             <ul
               role="list"
-              className="xl:hidden border-b border-zinc-800 w-full mx-auto max-w-2xl lg:max-w-full py-8 col-span-4 grid grid-cols-2 lg:grid-cols-3"
+              className="xl:hidden border-b dark:border-zinc-800 border-zinc-200 w-full mx-auto max-w-2xl lg:max-w-full col-span-4 grid grid-cols-2 lg:grid-cols-3"
             >
               {authors?.map((a) => (
                 <li key={`${a?.id}-author`} className="py-4 w-full">
@@ -83,7 +92,7 @@ export function BlogBase(props: BlogBaseProps) {
                       alt=""
                       height={80}
                       width={80}
-                      className="h-12 w-12 mr-3 flex-none rounded-full ring-1 dark:ring-white/30 ring-black/30"
+                      className="sm:h-12 sm:w-12 h-8 w-8 sm:mr-3 mr-1.5 flex-none rounded-full ring-1 dark:ring-white/30 ring-black/30"
                     />
                     <div className="flex flex-col">
                       <h3 className="flex-auto truncate text-sm font-semibold leading-6 dark:text-white text-black">
@@ -113,9 +122,9 @@ export function BlogBase(props: BlogBaseProps) {
 
             <aside className="hidden xl:block xl:col-span-1">
               <header className="flex items-center justify-between px-4 pb-4 sm:px-6 sm:pb-6 pt-16 lg:px-8">
-                <h2 className="text-base leading-7 text-zinc-600 dark:text-zinc-400 ">
+                <h3 className="text-base leading-7 text-zinc-600 dark:text-zinc-400 ">
                   Posted by
-                </h2>
+                </h3>
               </header>
               <ul role="list" className="">
                 {authors?.map((a) => (
--- apps/org/app/(marketing)/(blog.components)/tags/prose.tsx
+++ apps/org/app/(marketing)/(blog.components)/tags/prose.tsx
@@ -6,7 +6,7 @@ export function Prose({ as: Component = "div", className, ...props }: any) {
       className={cn(
         className,
         "prose dark:prose-invert",
-        "prose-p:text-lg prose-p:tracking-tight prose-p:leading-7 dark:prose-p:text-zinc-300 prose-p:text-zinc-800",
+        "xl:prose-p:text-lg prose-p:text-base prose-p:tracking-tight prose-p:leading-7 dark:prose-p:text-zinc-300 prose-p:text-zinc-800",
         "prose-li:text-lg prose-li:tracking-tight prose-li:font-medium prose-li:leading-7 dark:prose-li:text-zinc-300 prose-li:text-zinc-800"
       )}
       {...props}
